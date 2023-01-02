0903996843cb8076f5f9a16fe6a89363dfeacde2
vipin Patidar <vipinpatidar5@gmail.com>
2023-01-02 21:47:38 +0530

    initial commit

---
+++ js/portfolio.js
@@ -0,0 +1,134 @@
+filterSelection("all");
+function filterSelection(btnArea) {
+  let column = document.getElementsByClassName("column");
+
+  if (btnArea === "all") btnArea = "";
+  for (let i = 0; i < column.length; i++) {
+    removeClass(column[i], "showPro");
+    if (column[i].className.indexOf(btnArea) > -1)
+      addClass(column[i], "showPro");
+  }
+}
+
+function addClass(element, name) {
+  let arrayColumn = element.className.split(" ");
+  let arrayClass = name.split(" ");
+
+  arrayClass.forEach((arrayTwo) => {
+    if (arrayColumn.indexOf(arrayTwo) == -1) {
+      element.className += " " + arrayTwo;
+    }
+  });
+}
+
+function removeClass(element, name) {
+  let arrayOne = element.className.split(" ");
+  let arrayTwo = name.split(" ");
+
+  for (let i = 0; i < arrayTwo.length; i++) {
+    while (arrayOne.indexOf(arrayTwo[i]) > -1) {
+      arrayOne.splice(arrayOne.indexOf(arrayTwo[i]), 1);
+    }
+  }
+  element.className = arrayOne.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+let btnContainer = document.getElementById("myBtnContainer");
+let btns = btnContainer.getElementsByClassName("btn");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+///////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+// Get the button that opens the modal
+let btn = document.querySelectorAll(".myBtn");
+btn.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    const btnId = btn.dataset.btn;
+    document.querySelector(`[data-modal="${btnId}"]`).classList.add("showPro");
+  });
+});
+
+let span = document.querySelectorAll(".close");
+// console.log(span);
+span.forEach((X) => {
+  X.addEventListener("click", function (e) {
+    e.target.parentElement.parentElement.classList.remove("showPro");
+  });
+});
+
+let show = document.querySelector(".showPro");
+window.addEventListener("click", function (event) {
+  event.target.classList.remove("showPro");
+  // console.log(event.target);
+});
+
+// window.onclick = function (event) {
+//   if (event.target == show) {
+//     show.style.display = "none";
+//   }
+// };
+// //////////////////////////////////// top button //////////////////////////////////////
+
+let scrollBtn = document.querySelector("#myBtnPort");
+
+// window.addEventListener("onscroll", () => {
+//   scrollUp();
+//   // goOnTop();
+//   console.log("fdh");
+// });
+window.onscroll = function () {
+  scrollUp();
+};
+function scrollUp() {
+  if (
+    document.body.scrollTop > 400 ||
+    document.documentElement.scrollTop > 400
+  ) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// * Const //////////////////////////////////////////
+// changing nav color according to the current page
+
+const sections = document.querySelectorAll("section");
+
+function callBackFun(entries) {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0) {
+      clickLinks.forEach((link) => {
+        link.children[0].style.color = "white";
+      });
+      const x = document.querySelectorAll(`[href="#${entry.target.id}"]`);
+      x.forEach((link) => {
+        link.style.color = "#20c997";
+      });
+    }
+  });
+}
+
+const options = {
+  root: null,
+  thorshold: 0,
+  rootMargin: "-300px",
+};
+
+const observerSection = new IntersectionObserver(callBackFun, options);
+
+sections.forEach((section) => {
+  observerSection.observe(section);
+});
---
+++ js/script.js
@@ -0,0 +1,160 @@
+// Aos
+AOS.init();
+
+// /////////////////////////////////////
+// function myFunction() {
+//   let element = document.body;
+//   element.classList.toggle("dark");
+// }
+// /////////////////////////////////
+class Typewriter {
+  constructor(txtElement, words, wait = 2500) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = " "; //runing word in area
+    this.wordIndex = 0; //which word we are on accroding to formatted array
+    this.wait = parseInt(wait, 10); // 10 base value
+    this.type(); // a method
+    this.isDeleting = false; // when it go back where isdeleting = true
+  }
+
+  type() {
+    // Curremt index of word
+    const Current = this.wordIndex % this.words.length;
+
+    // console.log(this.wordIndex , this.words.length , Current)
+
+    // Get full text fo current index word
+    const fullText = this.words[Current];
+    //  console.log(fullText)- developer
+
+    // check if deleting
+    if (this.isDeleting) {
+      // if thid deleting the remove char
+      this.txt = fullText.substring(0, this.txt.length - 1); // subtract -1
+
+      // console.log(this.txt , fullText , this.txt.length );
+    } else {
+      //if it is not deleting Add char
+      this.txt = fullText.substring(0, this.txt.length + 1); //adding +1
+    }
+
+    //  get output of this.txt or adding in to dom span
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    //Type speed , it is vary according to situation
+    let typeSpeed = 300;
+
+    // Deleteing check and mank it faster
+    if (this.isDeleting) {
+      typeSpeed /= 2; // increasing deleting speed typespeed/2
+    }
+
+    //  If word is complete by matching with this.txt and fullText
+    if (!this.isDeleting && this.txt === fullText) {
+      // make pause at end
+      typeSpeed = this.wait;
+      // set delete to true
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      //this is the condition where our all word get deleted then we change the new word
+      this.isDeleting = false;
+      // move on next word
+      this.wordIndex++;
+      // pause before start typing
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed); // for continuous processing of delete and type of data- words
+  }
+}
+
+// initialize on DoM load
+document.addEventListener("DOMContentLoaded", init);
+
+// initialize App
+
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+
+  // inti TypeWriter
+  new Typewriter(txtElement, words, wait);
+}
+
+// /////////////////////////////////// Menu.js ////////////////////////////////////////////////////////////////////////////
+
+//Select Dom items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+
+const navItems = document.querySelectorAll(".nav-item");
+const clickLinks = document.querySelectorAll(".clicked-link");
+
+// Set Initial State of menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    document.body.style.overflow = "hidden";
+    navItems.forEach((e) => {
+      e.classList.add("show");
+    });
+    //set menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    document.body.style.overflow = "visible";
+    navItems.forEach((e) => {
+      e.classList.remove("show");
+    });
+
+    //set menu state
+    showMenu = false;
+  }
+}
+
+clickLinks.forEach((item, i) => {
+  item.addEventListener("click", function (e) {
+    clickLinks.forEach((link) => {
+      link.children[0].style.color = "white";
+    });
+    // item.children[0].style.color = "#20c997";
+    setTimeout(() => {
+      const x = document.querySelectorAll(`[href="${window.location.hash}"]`);
+      x.forEach((link) => {
+        console.log(link);
+        link.style.color = "#20c997";
+      });
+    }, 0);
+    console.log(item);
+    item.classList.contains("show") ? toggleMenu() : "";
+    // toggleMenu();
+  });
+});
+
+window.onload = function () {
+  // window.location = window.location.href;
+  window.location.hash ? (window.location = window.location.href) : "";
+  // clickLinks.forEach((link) => {
+  //   link.children[0].style.color = "white";
+  // });
+  setTimeout(() => {
+    const x = document.querySelectorAll(`[href="${window.location.hash}"]`);
+    x.forEach((link) => {
+      link.style.color = "#20c997";
+    });
+  }, 0);
+};
